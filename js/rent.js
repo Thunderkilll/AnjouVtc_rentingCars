e6e1d8c16c9bbfbe7fbbfb57c9e2c5513c544e08
Khaled_guesmi <khaled.guesmi@esprit.tn>
2019-12-28 15:40:10 +0100

    custum alert

--- js/rent.js
+++ js/rent.js
@@ -1,9 +1,13 @@
-
+ 
+var alert = document.getElementById("danger");
+var success = document.getElementById("success");
 
 (function($) {
 
 var rent = function() {
     $("#rent_btn").on('click', function(e) {
+      
+
         console.log(' renting btn clicked ');
         var today = new Date();
         console.log(today.toTimeString());
@@ -12,29 +16,70 @@ var rent = function() {
         var pickup_date =  document.getElementById("book_pick_date").value ;
         var drop_date =  document.getElementById("book_off_date").value ;
         var time_calender =  document.getElementById("time_pick").value ;
+
         if (location_start =="" || location_drop =="" || pickup_date =="" || drop_date =="" || time_calender ==""  ) {
             if (location_start =="" && location_drop =="" && pickup_date =="" && drop_date =="" && time_calender =="" ) {
                 alert('please complete the informations in the form');
             }else{
                 if (location_start =="") {
-                    document.getElementById("pich_up").style.backgroundColor = "#fba000";
-                    alert('this location input must not  be empty');
+                    //show alert visibility 
+                     alert.style.visibility="visible";
+                   //create alert 
+                   alert.innerHTML="<strong> Erreur  </strong>   - Champ de start location est vide  ";
+                   //show alert 
+                    window.setTimeout(function() {
+                        $("#danger").fadeTo(500, 0).slideUp(500, function(){
+                            $(this).remove(); 
+                        });
+                    }, 4000);
                 }
                 if (location_drop =="") {
-                    document.getElementById("pich_up").style.backgroundColor = "#fba000";
-                    alert('this location input must not  be empty');
+                    //visibility 
+                     alert.style.visibility="visible";
+                   //create alert 
+                   alert.innerHTML="<strong> Erreur  </strong>   - Champ de drop location est vide  ";
+                   //show alert 
+                    window.setTimeout(function() {
+                        $("#danger").fadeTo(500, 0).slideUp(500, function(){
+                            $(this).remove(); 
+                        });
+                    }, 4000);
                 }
                 if (pickup_date =="") {
-                    document.getElementById("pich_up").style.backgroundColor = "#fba000";
-                    alert('this date input must not  be empty');
+                    //visibility 
+                       alert.style.visibility="visible";
+                   //create alert 
+                   alert.innerHTML="<strong> Erreur  </strong>   - Champ de start date  est vide  ";
+                   //show alert 
+                    window.setTimeout(function() {
+                        $("#danger").fadeTo(500, 0).slideUp(500, function(){
+                            $(this).remove(); 
+                        });
+                    }, 4000);
                 }
                 if (drop_date =="") {
-                    document.getElementById("pich_up").style.backgroundColor = "#fba000";
-                    alert('this date input must not  be empty');
+                    //visibility 
+                      alert.style.visibility="visible";
+                    //create alert 
+                   alert.innerHTML="<strong> Erreur  </strong>   - Champ de drop date  est vide  ";
+                   //show alert 
+                    window.setTimeout(function() {
+                        $("#danger").fadeTo(500, 0).slideUp(500, function(){
+                            $(this).remove(); 
+                        });
+                    }, 4000);
                 }
                 if (time_calender =="") {
-                    document.getElementById("pich_up").style.backgroundColor = "#fba000";
-                    alert('this calender date input must not  be empty');
+                    //visibility 
+                      alert.style.visibility="visible";
+                    //create alert 
+                   alert.innerHTML="<strong> Erreur  </strong>   - Champ de calendrier est vide  ";
+                   //show alert 
+                    window.setTimeout(function() {
+                        $("#danger").fadeTo(500, 0).slideUp(500, function(){
+                            $(this).remove(); 
+                        });
+                    }, 4000);
                 }
             }
            
@@ -44,13 +89,31 @@ var rent = function() {
             console.log('pick date :'+ pickup_date);
             console.log('drop date :'+ drop_date);
             console.log('date on calender :'+ time_calender);
-        }
+ 
         
         if (location_start.length < 2 ) {
-            alert('you need at least 3 lettres for a place ');
+            //visibility 
+              alert.style.visibility="visible";
+            //create alert 
+            alert.innerHTML="<strong> Erreur  </strong>   -  le champ de location start doit avoir plus que 2 lettres et pas d'espaces  ";
+            //show alert 
+             window.setTimeout(function() {
+                 $("#danger").fadeTo(500, 0).slideUp(500, function(){
+                     $(this).remove(); 
+                 });
+             }, 4000);
         }
         if (location_drop.length < 2 ) {
-            alert('you need at least 3 lettres for a place ');
+            //visibility 
+             alert.style.visibility="visible"; ;
+            //create alert 
+            alert.innerHTML="<strong> Erreur  </strong>   - le champ de location drop doit avoir plus que 2 lettres et pas d'espaces   ";
+            //show alert 
+             window.setTimeout(function() {
+                 $("#danger").fadeTo(500, 0).slideUp(500, function(){
+                     $(this).remove(); 
+                 });
+             }, 4000);
         }
 
         var date1 = $("#book_pick_date").datepicker('getDate').getTime(); 
@@ -59,11 +122,43 @@ var rent = function() {
         var same = today.getTime() === date1 ;
         var notSame = today.getTime() !== date1 ;
         var before = today.getTime() <= date1 ;
+        console.log("today and start day are the same : " + same);
+        console.log("today and start day aren't the same   : " + notSame);
 
         console.log(dates.compare(date1 , date2));
         
-        console.log("today and start day are the same : " + same);
-        console.log("today and start day aren't the same   : " + notSame);
+        if (dates.compare(date1 , date2) == 1) {
+            //visibility 
+              alert.style.visibility="visible"; ;  
+           //create alert 
+           alert.innerHTML="<strong> Erreur  </strong>   - la date de départ depasse la date de fin  ";
+           //show alert 
+            window.setTimeout(function() {
+                $("#danger").fadeTo(500, 0).slideUp(500, function(){
+                    $(this).remove(); 
+                });
+            }, 4000);
+        }
+        //visibility 
+        success.style.visibility =  "visible" ;
+        //create alert 
+        success.innerHTML=" <strong>Yeppi</strong> - vous avez réserver avec success  ";
+        //show alert 
+         window.setTimeout(function() {
+             $("#success").fadeTo(500, 0).slideUp(500, function(){
+                 $(this).remove(); 
+             });
+         }, 4000);
+
+    }
+
+
+    });
+    };
+    rent();
+ 
+      
+})(jQuery);
         
         //var dd = String(today.getDate()).padStart(2, '0');
        // var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -78,14 +173,6 @@ var rent = function() {
        //console.log("the same date : " + same);
        //console.log("not the same date : " + notSame);
         
-
-    });
-    };
-    rent();
- 
-      
-})(jQuery);
-
 
  
     var dates = {
